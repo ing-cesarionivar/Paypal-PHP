0377172e5e290311637737bb1fb92434ce429443
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-09-16 19:22:11 -0400

    Capturando los datos del formulario

---
+++ pagar.php
@@ -0,0 +1,16 @@
+<?php
+
+    if(!isset($_POST['producto'], $_POST['precio'])) {
+        exit("Hubo un error");
+    }
+
+    require('config.php');
+
+    $producto = htmlspecialchars($_POST['producto']);
+    $precio = htmlspecialchars($_POST['precio']);
+    $precio = (int) $precio;
+    var_dump($precio);
+    $envio = 0;
+    $total = $precio + $envio;
+
+?>
