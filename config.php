48f7e27422250555f4911c69d6f9edfc5abb1f02
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-09-16 19:16:31 -0400

    Configuración Paypal Api

--- config.php
+++ config.php
@@ -1,4 +1,13 @@
 <?php
     require('paypal/autoload.php');
+
+    $apiContext = new \PayPal\Rest\ApiContext(
+        new \PayPal\Auth\OAuthTokenCredential(
+            'AdtqJArL_k3JeOhBRSLEC1qWoS7dl3CYu86lBJ72yY2R2PLlEKYFCdvH0F7MRbU_MTIT2Cx37uscGAWq',     // ClientID
+            'EH0Fdeloh-s7qTn07drvAwexLYjeuuJemRpoDL0a9puhXfa-mly-SLkAqJiETV8SQ9TEPETtvismtn1C'      // ClientSecret
+        )
+    );
+
+    
     
 ?>
